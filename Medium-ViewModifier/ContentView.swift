b56ce87e3479db1049aec02159c3b68e15509f42
Kristóf Kálai <kristof.kalai@supercharge.io>
2023-12-16 21:43:23 +0100

    feat: update SegmentedControl with a viewmodifier

--- Medium-ViewModifier/ContentView.swift
+++ Medium-ViewModifier/ContentView.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+struct BorderModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .border(.red)
+    }
+}
+
 struct ContentView: View {
     enum Tab: String, CaseIterable {
         case first
@@ -16,34 +23,51 @@ struct ContentView: View {
     @State private var selectedTab: Tab = .first
 
     var body: some View {
-        SegmentedControl(
-            tabs: Tab.allCases,
-            selectedTab: $selectedTab,
-            title: \.rawValue
-        ) { tab in
-            switch tab {
-            case .first: Color.red
-            case .second: Color.blue
+        VStack {
+            SegmentedControl(
+                tabs: Tab.allCases,
+                selectedTab: $selectedTab,
+                title: \.rawValue
+            ) { tab in
+                switch tab {
+                case .first: Color.red
+                case .second: Color.blue
+                }
+            }
+
+            SegmentedControl(
+                tabs: Tab.allCases,
+                selectedTab: $selectedTab,
+                title: \.rawValue,
+                modifier: BorderModifier()
+            ) { tab in
+                switch tab {
+                case .first: Color.red
+                case .second: Color.blue
+                }
             }
         }
     }
 }
 
-struct SegmentedControl<Tab: Hashable, Content: View>: View {
+struct SegmentedControl<Tab: Hashable, Content: View, Modifier: ViewModifier>: View {
     private let tabs: [Tab]
     @Binding private var selectedTab: Tab
     private let title: (Tab) -> String
+    private let modifier: Modifier
     @ViewBuilder private let content: (Tab) -> Content
 
     init(
         tabs: [Tab],
         selectedTab: Binding<Tab>,
         title: @escaping (Tab) -> String,
+        modifier: Modifier,
         @ViewBuilder content: @escaping (Tab) -> Content
     ) {
         self.tabs = tabs
         self._selectedTab = selectedTab
         self.title = title
+        self.modifier = modifier
         self.content = content
     }
 
@@ -55,9 +79,27 @@ struct SegmentedControl<Tab: Hashable, Content: View>: View {
                 }
             }
             .pickerStyle(.segmented)
+            .modifier(modifier)
 
             content(selectedTab)
         }
+    }
+}
+
+extension SegmentedControl where Modifier == EmptyModifier {
+    init(
+        tabs: [Tab],
+        selectedTab: Binding<Tab>,
+        title: @escaping (Tab) -> String,
+        @ViewBuilder content: @escaping (Tab) -> Content
+    ) {
+        self.init(
+            tabs: tabs,
+            selectedTab: selectedTab,
+            title: title,
+            modifier: EmptyModifier(),
+            content: content
+        )
     }
 }
 
